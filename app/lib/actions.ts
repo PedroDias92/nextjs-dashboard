b242e25900feac11cdb1aa6e583c50fd46c6d56d
Pedro Dias <pedro.dias@waitrose.co.uk>
2024-02-02 14:51:43 +0000

    chapter 13 - error handling

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -27,17 +27,22 @@ export async function createInvoice(formData: FormData) {
   const amountInCents = amount * 100;
   const date = new Date().toISOString().split('T')[0];
 
-  await sql`
+  try {
+    await sql`
     INSERT INTO invoices (customer_id, amount, status, date)
     VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
   `;
+  } catch (err) {
+    return {
+      message: 'Database Error: Failed to Create Invoice.',
+    };
+  }
 
   /*   Since you're updating the data displayed in the invoices route,
   you want to clear this cache and trigger a new request to the server.
   You can do this with the revalidatePath function from Next.js:
    */
 
-  console.log({ customerId, amount, status, date });
   revalidatePath('/dashboard/invoices');
 
   redirect('/dashboard/invoices');
@@ -52,17 +57,25 @@ export async function updateInvoice(id: string, formData: FormData) {
 
   const amountInCents = amount * 100;
 
-  await sql`
-      UPDATE invoices
-      SET customer_id = ${customerId}, amount = ${amountInCents}, status = ${status}
-      WHERE id = ${id}
-    `;
-
+  try {
+    await sql`
+        UPDATE invoices
+        SET customer_id = ${customerId}, amount = ${amountInCents}, status = ${status}
+        WHERE id = ${id}
+      `;
+  } catch (error) {
+    return { message: 'Database Error: Failed to Update Invoice.' };
+  }
   revalidatePath('/dashboard/invoices');
   redirect('/dashboard/invoices');
 }
 
 export async function deleteInvoice(id: string) {
-  await sql`DELETE FROM invoices WHERE id = ${id}`;
-  revalidatePath('/dashboard/invoices');
+  try {
+    await sql`DELETE FROM invoices WHERE id = ${id}`;
+    revalidatePath('/dashboard/invoices');
+    return { message: 'Deleted Invoice.' };
+  } catch (error) {
+    return { message: 'Database Error: Failed to Delete Invoice.' };
+  }
 }
